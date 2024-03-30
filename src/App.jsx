e6a7ee044f74de93c8d6b495bcdfb9c53f701e0b
Gustavo Duarte <gd245149@fi365.ort.edu.uy>
2024-03-30 19:33:04 -0300

    Add Material Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@material-tailwind/react";
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <h1 className="text-3xl font-bold underline uppercase">
-      Hello world!
-    </h1>
-  )
+  return <Button>Button</Button>;
 }
 
 export default App
